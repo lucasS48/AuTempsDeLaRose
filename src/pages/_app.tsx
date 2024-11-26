826807a0954d9792373ce35f9e0728c950e911ea
lucasS48 <luccobra48@gmail.com>
2024-11-26 15:14:46 +0100

    footerAdd

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,10 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Footer from "@/components/Footer";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return  <>
+  <Component {...pageProps} />
+  <Footer />
+</>;
 }
