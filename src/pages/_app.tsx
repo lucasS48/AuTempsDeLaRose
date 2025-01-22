5edce36ab9470c231b40401976ce8c52a469e1d8
lucasS48 <luccobra48@gmail.com>
2025-01-22 23:06:42 +0100

    AnimateWait

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,6 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
-import { AnimatePresence } from "framer-motion";
 import { useRouter } from "next/router";
 import Head from "next/head";
 import { SpeedInsights } from "@vercel/speed-insights/next";
@@ -13,7 +12,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>Au Temps de la Rose - Conseils pour vos Plantes</title>
+        <title>Au Temps de la Rose</title>
         <meta
           name="description"
           content="Découvrez nos conseils pour entretenir vos plantes et les garder en pleine santé."
@@ -22,9 +21,7 @@ export default function App({ Component, pageProps }: AppProps) {
       </Head>
       <Header />
       <div className="pt-16">
-        <AnimatePresence mode="wait" initial={false}>
           <Component {...pageProps} key={router.route} />
-        </AnimatePresence>
       </div>
 
       <Footer />
